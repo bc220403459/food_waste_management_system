60fa2e9697e6b7ae8b676a18d79210db7372bfa7
codeofhassan <theultimatehassan@gmail.com>
2024-06-03 17:00:36 +0500

    3rd June

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
-<!doctype html>
 <?php
-include('./functions.php');
-$image=fileNameWithBasePath("images/banner.png");
+include ('./functions.php');
+include (fileNameWithBasePath('/includes/header.php'));
 ?>
-<img src="<?php $image ?>"/>
-<?php exit ?>
 <html lang="en">
 
 <head>
@@ -14,26 +11,27 @@ $image=fileNameWithBasePath("images/banner.png");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
-      body{
-        background-color:#F1ECE7;
-      }
-    .divider:after,
-    .divider:before {
-        content: "";
-        flex: 1;
-        height: 1px;
-        background: #eee;
-    }
-
-    .h-custom {
-        height: calc(100% - 73px);
-    }
-
-    @media (max-width: 450px) {
-        .h-custom {
-            height: 100%;
+        body {
+            background-color: #F1ECE7;
         }
-    }
+
+        .divider:after,
+        .divider:before {
+            content: "";
+            flex: 1;
+            height: 1px;
+            background: #eee;
+        }
+
+        .h-custom {
+            height: calc(100% - 73px);
+        }
+
+        @media (max-width: 450px) {
+            .h-custom {
+                height: 100%;
+            }
+        }
     </style>
 </head>
 
@@ -42,107 +40,39 @@ $image=fileNameWithBasePath("images/banner.png");
         <div class="container-fluid h-custom">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-md-9 col-lg-6 col-xl-5">
-                    <!-- <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                        class="img-fluid" alt="Sample image"> -->
-                        <img src="banner.png"
-                        class="img-fluid" alt="Sample image">
+                    <img src="images/banner.png" class="img-fluid" alt="Sample image">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                <h1 class="text-center mb-1" style="text-align:center !important">Login</h1>
-                    <form>
-                        <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                        <div class="text-center">
-                        
-                        </div>  
-                          <div class="mt-5">
-                            &nbsp;
-                          </div>
-                            <!-- <p class="lead fw-normal mb-0 me-3">Sign in with</p>
-                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-primary btn-floating mx-1">
-                                <i class="fab fa-facebook-f"></i>
-                            </button>
-
-                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-primary btn-floating mx-1">
-                                <i class="fab fa-twitter"></i>
-                            </button>
-
-                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-primary btn-floating mx-1">
-                                <i class="fab fa-linkedin-in"></i>
-                            </button> -->
-                        </div>
-
-                        <!-- <div class="divider d-flex align-items-center my-4">
-                            <p class="text-center fw-bold mx-3 mb-0">Or</p>
-                        </div> -->
-
-                        <!-- Email input -->
+                    <h1 class="text-center mb-5" style="text-align:center !important">Login</h1>
+                    <form action="<?php fileNameWithBasePath('/login.php') ?>" method="post">
                         <div data-mdb-input-init class="form-outline mb-4">
                             <input type="email" id="form3Example3" class="form-control form-control-lg"
                                 placeholder="Enter a valid email address" />
-                            <!-- <label class="form-label" for="form3Example3">Email address</label> -->
                         </div>
-
-                        <!-- Password input -->
                         <div data-mdb-input-init class="form-outline mb-3">
                             <input type="password" id="form3Example4" class="form-control form-control-lg"
                                 placeholder="Enter password" />
-                            <!-- <label class="form-label" for="form3Example4">Password</label> -->
-                        </div>
-
-                        <div class="d-flex justify-content-between align-items-center">
-                            <!-- Checkbox -->
-                            <div class="form-check mb-0">
-                                <!-- <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" /> -->
-                                <!-- <label class="form-check-label" for="form2Example3">
-                                    Remember me
-                                </label> -->
-                            </div>
-                            <!-- <a href="#!" class="text-body">Forgot password?</a> -->
                         </div>
 
                         <div class="text-center text-lg-start mt-2 pt-2">
-                          <div class="text-center">
-                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-success btn-lg"
-                                style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-                                </div>
-                            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                                    class="link-danger">Register</a></p>
-                        </div>
-
+                            <div class="text-center">
+                                <button type="button" data-mdb-button-init data-mdb-ripple-init
+                                    class="btn btn-success btn-lg"
+                                    style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                            </div>
                     </form>
+                    <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="./pages/sign-up.php"
+                            class="link-danger">Register</a></p>
                 </div>
             </div>
         </div>
-        <!-- <div
-            class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-            <div class="text-white mb-3 mb-md-0">
-                Copyright © 2020. All rights reserved.
-            </div>
-            <div>
-                <a href="#!" class="text-white me-4">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="#!" class="text-white me-4">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a href="#!" class="text-white me-4">
-                    <i class="fab fa-google"></i>
-                </a>
-                <a href="#!" class="text-white">
-                    <i class="fab fa-linkedin-in"></i>
-                </a>
-            </div>
-        </div> -->
+        </div>
     </section>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+        </script>
 </body>
 
 </html>
