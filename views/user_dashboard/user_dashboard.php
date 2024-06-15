4fc857456e7099b3da30a841a6b036c7aab9e7b7
codeofhassan <theultimatehassan@gmail.com>
2024-06-15 16:19:38 +0500

    till 15th june

--- views/user_dashboard/user_dashboard.php
+++ views/user_dashboard/user_dashboard.php
@@ -37,7 +37,6 @@ if (!isset($_SESSION['username']))
   20%, 60% { transform: translateX(-5px); }
   40%, 80% { transform: translateX(10px); }
 }
-
 </style>
 <?php 
   $toExpire=getExpiringFoodItems();
@@ -102,16 +101,17 @@ if (!isset($_SESSION['username']))
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="expiryReminderLabel">Expiry Reminders</h5>
+                    <h4 class="modal-title" id="expiryReminderLabel">Expiry Reminders</h4>
                     <div class="text-end" style="margin-left:auto !important">
                     <button type="button btn-outline-danger" class="close" data-dismiss="modal" aria-label="Close">
+                   
                         <span aria-hidden="true">&times;</span>
                     </button>
                     </div>
                 </div>
                 <div class="modal-body">
-                    <div class="text-center">
-                      <table class="table">
+                  <div class="text-center">
+                    <table class="table">
                         <tr>
                           <th>Item Name</th>
                           <th>Quantity Left</th>
@@ -160,14 +160,47 @@ if (!isset($_SESSION['username']))
                     </div>
                 </div>
                 <div class="modal-footer">
+                    <button type="button" class="btn btn-sm btn-primary reminderSettings">Settings</button> 
+                    <script>
+                     $(document).ready(function() {
+  $('#expiryReminder').on('shown.bs.modal', function () {
+    $('.btn-primary').click(function() {
+      // Open reminderSettings modal
+      $('#reminderSettings').modal('show');
+    });
+  });
+});
+
+                    </script>
                     <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="reminderSettings" tabindex="-1" aria-labelledby="reminderSettingsLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="reminderSettingsLabel">Reminder Settings</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>This is the reminder settings modal content. You can add options to configure notification frequency, expiry thresholds, etc.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
     </button>
   </div>
   <p>
