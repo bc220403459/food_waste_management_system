85523a9556dda5e43c709143882e594088c8b835
Hassan_HP_15 <fypforadpcs2024@gmail.com>
2024-06-24 03:54:01 +0500

    header file changed

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Food Waste Management System</title>
+  <title>Food Waste Reduction System</title>
   <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
   <link rel="icon" type="image/x-icon" href="../../images/favicon.ico">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
